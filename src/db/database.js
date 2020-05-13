d0e55418b2a7a52ca0203bc9a2a5f2b2302f508e
lakshmi/achuth <coderubble@gmail.com>
2020-05-14 08:42:03 +1000

    Changed DB options

--- src/db/database.js
+++ src/db/database.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-mongoose.set('debug', true);
+mongoose.set('debug', process.env.DEBUG_QUERY);
 module.exports = mongoose;
